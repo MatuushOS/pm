03c17067a6c7bdfd73a3c906c69dbe1b0b709bd9
Matus Mastena <Shadiness9530@pm.me>
2024-11-18 18:49:59 +0100

    Write tests

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ mod tests {
 
     #[test]
     fn build_all() {
-        for f in read_dir("examples").unwrap() {
+        for f in read_dir(Path::new(".").join("examples")).unwrap() {
             let script = f.unwrap().path();
             let x = Path::new(script.as_path()).to_str().unwrap();
             let x1 = x.trim_end_matches(".mt");
