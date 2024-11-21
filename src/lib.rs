564c10b16bd511edf20c68c89d07bfcb52a70d83
Matus Mastena <Shadiness9530@pm.me>
2024-11-21 15:17:10 +0100

    Add more functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,16 @@
 #[cfg(test)]
 mod tests {
     use std::{fs::read_dir, path::Path, process::Command};
+    use log::info;
+    
     #[test]
     fn build_all() {
+        colog::init();
         for f in read_dir(Path::new(".").join("examples")).unwrap() {
             for scr in read_dir(Path::new(&f.unwrap().path())).unwrap() {
                 let script = scr.unwrap().path();
                 let x = Path::new(script.as_path()).to_str().unwrap();
+                info!("Opening {x}");
                 let x1 = x.trim_end_matches(".mt");
                 let cmd = Command::new("target/debug/pm")
                     .args(["install", x1])
